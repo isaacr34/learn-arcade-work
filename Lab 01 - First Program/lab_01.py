02c12b5d00e831c5b8dd54fb72bc18d3a17c145b
isaacr34 <isaac.roe@my.simpson.edu>
2020-09-03 10:17:27 -0500

    Work on lab 1

--- Lab 01 - First Program/lab_01.py
+++ Lab 01 - First Program/lab_01.py
@@ -1,1 +1,4 @@
-Hi
+print("\tWow this a pretty cool thing.")
+print("It probably does a lot of neat stuff.")
+print("I can't wait to learn more.")
+print("This is only the beginning.")
---
+++ Testing/test.py
@@ -0,0 +1,16 @@
+import arcade
+
+# This is a comment
+
+# wow that is super neat
+
+"""
+wow another comment
+"""
+arcade.open_window(800, 600, "My Sample Window")
+
+arcade.set_background_color((117, 237, 255))
+arcade.start_render()
+arcade.finish_render()
+arcade.run()
+
