91335a8084f2f0136ed9b9efc85cf3b119f99d35
isaacr34 <isaac.roe@my.simpson.edu>
2020-09-03 10:24:26 -0500

    Work on lab 1

--- Lab 01 - First Program/lab_01.py
+++ Lab 01 - First Program/lab_01.py
@@ -1,4 +1,4 @@
-print("\tWow this a pretty cool thing.")
+print("\tWow this is a pretty cool thing.")
 print("It probably does a lot of neat stuff.")
 print("I can't wait to learn more.")
 print("This is only the beginning.")
