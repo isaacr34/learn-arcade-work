b32895f127626505c188fe4664227de4fcdfb800
isaacr34 <isaac.roe@my.simpson.edu>
2020-10-15 14:03:09 -0500

    Work on lab 6

--- Lab 06 - Text Adventure/lab_06.py
+++ Lab 06 - Text Adventure/lab_06.py
@@ -1,64 +1,110 @@
 class Room:
     def __init__(self, description, north, east, south, west):
-        self.description = ""
-        self.north = 0
-        self.east = 0
-        self.south = 0
-        self.west = 0
+        self.description = description
+        self.north = north
+        self.east = east
+        self.south = south
+        self.west = west
 
 
 def main():
     room_list = []
-    room = Room("You are in the Storage Room of a house.",
+    room = Room("You are in the storage room.\nThere is a door to the south that leads to part of the living room.",
                 None,
                 None,
                 3,
                 None)
-    room_list.append(Room)
-    room = Room("You are in a bedroom of a house.",
+    room_list.append(room)
+    room = Room("You are in the north bedroom.\nThere is a door to the south that leads to part of the living room.",
                 None,
                 None,
                 4,
                 None)
-    room_list.append(Room)
-    room = Room("You are in the Entrance Hall to a house.",
+    room_list.append(room)
+    room = Room("You are in the entryway to a house.\nIt leads to the east.",
                 None,
+                3,
                 None,
-                None,
-                3)
-    room_list.append(Room)
-    room = Room("You are in the West Hall of a house.",
+                None)
+    room_list.append(room)
+    room = Room("You are in the west part of the living room.\nThere are doors to the north, west, south, and to the "
+                "east\nis another part of the living room.",
                 0,
-                2,
+                4,
                 6,
-                4)
-    room_list.append(Room)
-    room = Room("You are in the East Hall of a house.",
+                2)
+    room_list.append(room)
+    room = Room("You are in the east part of the living room.\nThere are doors to the north, east, south, and to the "
+                "west\nis the other part of the living room.",
                 1,
+                5,
+                7,
+                3)
+    room_list.append(room)
+    room = Room("You are in the east bedroom.\nThere is a door to the west that leads to part of the living room.",
+                None,
+                None,
+                None,
+                4)
+    room_list.append(room)
+    room = Room("You are in the dining room.\nThere are doors to the north and east.",
                 3,
                 7,
-                5)
-    room_list.append(Room)
-    room = Room("You are in a bedroom in a house.",
-                None,
-                4,
                 None,
                 None)
-    room_list.append(Room)
-    room = Room("You are in the dining room of a house.",
-                3,
-                None,
-                None,
-                7)
-    room_list.append(Room)
-    room = Room("You are in the kitchen of a house.",
+    room_list.append(room)
+    room = Room("You are in the kitchen.\nThere are doors that lead to the north and to the west.",
                 4,
                 None,
                 None,
                 6)
-    room_list.append(Room)
-    current_room = 0
-    print(room_list, current_room)
+    room_list.append(room)
+    current_room = 2
+
+    done = False
+    while not done:
+        print()
+        print(room_list[current_room].description)
+        user_input = input("Where do you want to go? ")
+
+        if user_input.lower() == "n" or user_input.lower() == "north":
+            next_room = room_list[current_room].north
+            if next_room is None:
+                print()
+                print("You can't go that way.")
+            else:
+                current_room = next_room
+
+        elif user_input.lower() == "s" or user_input.lower() == "south":
+            next_room = room_list[current_room].south
+            if next_room is None:
+                print()
+                print("You can't go that way.")
+            else:
+                current_room = next_room
+
+        elif user_input.lower() == "e" or user_input.lower() == "east":
+            next_room = room_list[current_room].east
+            if next_room is None:
+                print()
+                print("You can't go that way.")
+            else:
+                current_room = next_room
+
+        elif user_input.lower() == "w" or user_input.lower() == "west":
+            next_room = room_list[current_room].west
+            if next_room is None:
+                print()
+                print("You can't go that way.")
+            else:
+                current_room = next_room
+
+        elif user_input.lower() == "q" or user_input.lower() == "quit":
+            done = True
+
+        else:
+            print()
+            print("Input not understood.")
 
 
 main()
--- Testing/test.py
+++ Testing/test.py
@@ -1,11 +1,2 @@
 import arcade
-import random
 
-SCREEN_WIDTH = 1200
-SCREEN_HEIGHT = 800
-
-
-class Coin(arcade.Sprite):
-
-    def reset(selfself):
-        self.bottom = SCREEN_HEIGHT
