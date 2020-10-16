0fb454ae0df4f354ed3c070ce219c7827b516915
isaacr34 <isaac.roe@my.simpson.edu>
2020-10-16 00:31:17 -0500

    Work on lab 8

--- Lab 08 - Sprites/lab_08.py
+++ Lab 08 - Sprites/lab_08.py
@@ -1,28 +1,68 @@
-""" Sprite Sample Program """
-
 import random
 import arcade
 
 # --- Constants ---
 SPRITE_SCALING_PLAYER = 0.5
-SPRITE_SCALING_COIN = 0.2
-COIN_COUNT = 50
+SPRITE_SCALING_GEM = 0.5
+SPRITE_SCALING_COAL = 0.25
+COAL_COUNT = 50
+GEM_COUNT = 50
 
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
 
 
+class Gem(arcade.Sprite):
+
+    def __init__(self, filename, sprite_scaling):
+
+        super().__init__(filename, sprite_scaling)
+
+        self.change_x = 0
+        self.change_y = 0
+
+    def update(self):
+
+        self.center_x += self.change_x
+        self.center_y += self.change_y
+
+        if self.left < 0:
+            self.change_x *= -1
+
+        if self.right > SCREEN_WIDTH:
+            self.change_x *= -1
+
+        if self.bottom < 0:
+            self.change_y *= -1
+
+        if self.top > SCREEN_HEIGHT:
+            self.change_y *= -1
+
+
+class Coal(arcade.Sprite):
+
+    def reset_pos(self):
+        self.left = random.randrange(SCREEN_HEIGHT)
+        self.center_x = SCREEN_WIDTH
+
+    def update(self):
+        self.center_x -= 1
+
+        if self.right < 0:
+            self.reset_pos()
+
+
 class MyGame(arcade.Window):
-    """ Our custom Window Class"""
 
     def __init__(self):
-        """ Initializer """
+
         # Call the parent class initializer
         super().__init__(SCREEN_WIDTH, SCREEN_HEIGHT, "Sprite Example")
 
         # Variables that will hold sprite lists
         self.player_list = None
-        self.coin_list = None
+        self.gem_list = None
+        self.coal_list = None
 
         # Set up the player info
         self.player_sprite = None
@@ -38,7 +78,8 @@ class MyGame(arcade.Window):
 
         # Sprite lists
         self.player_list = arcade.SpriteList()
-        self.coin_list = arcade.SpriteList()
+        self.gem_list = arcade.SpriteList()
+        self.coal_list = arcade.SpriteList()
 
         # Score
         self.score = 0
@@ -51,24 +92,36 @@ class MyGame(arcade.Window):
         self.player_list.append(self.player_sprite)
 
         # Create the coins
-        for i in range(COIN_COUNT):
+        for i in range(GEM_COUNT):
 
             # Create the coin instance
             # Coin image from kenney.nl
-            coin = arcade.Sprite("coin_01.png", SPRITE_SCALING_COIN)
+            gem = Gem("platformPack_item010.png", SPRITE_SCALING_GEM)
 
             # Position the coin
-            coin.center_x = random.randrange(SCREEN_WIDTH)
-            coin.center_y = random.randrange(SCREEN_HEIGHT)
+            gem.center_x = random.randrange(SCREEN_WIDTH)
+            gem.center_y = random.randrange(SCREEN_HEIGHT)
+            gem.change_x = random.randrange(-3, 4)
+            gem.change_y = random.randrange(-3, 4)
 
             # Add the coin to the lists
-            self.coin_list.append(coin)
+            self.gem_list.append(gem)
+
+        for i in range(COAL_COUNT):
+
+            coal = Coal("ore_coal.png", SPRITE_SCALING_COAL)
+
+            coal.center_x = random.randrange(SCREEN_WIDTH)
+            coal.center_y = random.randrange(SCREEN_HEIGHT)
+
+            self.coal_list.append(coal)
 
     def on_draw(self):
         """ Draw everything """
         arcade.start_render()
-        self.coin_list.draw()
+        self.gem_list.draw()
         self.player_list.draw()
+        self.coal_list.draw()
 
         # Put the text on the screen.
         output = f"Score: {self.score}"
@@ -80,6 +133,24 @@ class MyGame(arcade.Window):
         # Move the center of the player sprite to match the mouse x, y
         self.player_sprite.center_x = x
         self.player_sprite.center_y = y
+
+    def update(self, delta_time):
+
+        self.gem_list.update()
+
+        gems_hit_list = arcade.check_for_collision_with_list(self.player_sprite, self.gem_list)
+
+        for gem in gems_hit_list:
+            gem.remove_from_sprite_lists()
+            self.score += 1
+
+        self.coal_list.update()
+
+        coal_hit_list = arcade.check_for_collision_with_list(self.player_sprite, self.coal_list)
+
+        for coal in coal_hit_list:
+            coal.remove_from_sprite_lists()
+            self.score -= 1
 
 
 def main():
