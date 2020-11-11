d3b19ce9aaa024c42d400db79d3caf1d1bb2d767
isaacr34 <isaac.roe@my.simpson.edu>
2020-11-10 19:26:01 -0600

    Work on sorting worksheet

--- Testing/test.py
+++ Testing/test.py
@@ -17,3 +17,22 @@ def main():
 
 main()
 """
+my_list = [55, 41, 52, 68, 45, 27, 40, 25, 37, 26]
+
+print(my_list)
+
+temp = my_list[6]
+my_list[6] = my_list[7]
+my_list[7] = temp
+
+print(my_list)
+
+my_list = [27, 32, 18, 2, 11, 57, 14, 38, 19, 91]
+
+print(my_list)
+
+temp = my_list[0]
+my_list[0] = my_list[3]
+my_list[3] = temp
+
+print(my_list)
